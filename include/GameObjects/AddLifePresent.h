56e374f6f8954e1379257541fb5e09ca9bd7c627
OmerShim <omershim97@gmail.com>
2024-06-14 16:02:36 +0300

    Add files via upload

---
+++ include/GameObjects/AddLifePresent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "UnmoveableObject.h"
+#include "GameObjects/Mouse.h"
+
+
+class AddLifePresent : public UnmoveableObject {
+public:
+	using UnmoveableObject::UnmoveableObject;
+	virtual void handleCollision(GameObject&);
+	virtual void handleCollision(Mouse&);
+
+private:
+
+};
